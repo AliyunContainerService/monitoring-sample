5215be464bc82a7ef83d954fe90eb36602b5a783
Li Yi <denverdino@gmail.com>
2016-05-06 21:18:22 +0800

    Update image versions
    Change-Id: Iacfb62ece3d73192a4730224013c45fdd39053d6

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker build --pull -t registry.aliyuncs.com/acs-sample/google-cadvisor:v0.20 cadvisor/v0.20
+docker build --pull -t registry.aliyuncs.com/acs-sample/google-cadvisor:v0.22 cadvisor/v0.22
+docker build --pull -t registry.aliyuncs.com/acs-sample/google-cadvisor:v0.23 cadvisor/v0.23
+
+docker build --pull -t registry.aliyuncs.com/acs-sample/grafana:2.0 grafana/2.0
+docker build --pull -t registry.aliyuncs.com/acs-sample/grafana:2.6 grafana/2.6
+
+docker build --pull -t registry.aliyuncs.com/acs-sample/tutum-influxdb:0.8.8 influxdb/0.8.8
+docker build --pull -t registry.aliyuncs.com/acs-sample/tutum-influxdb:0.9 influxdb/0.9
+
+docker build --pull -t registry.aliyuncs.com/acs-sample/grafana-config:0.8 grafana-config/0.8
+docker build --pull -t registry.aliyuncs.com/acs-sample/grafana-config:0.9 grafana-config/0.9
